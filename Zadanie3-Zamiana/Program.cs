8f3683ce148524e2f4b8d29396868eadcbc6befc
ZawadzkiKonrad <zaw_kon@o2.pl>
2022-02-01 08:49:58 +0100

    komentarz1

---
+++ Zadanie3-Zamiana/Program.cs
@@ -0,0 +1,7 @@
+﻿String plik = System.IO.File.ReadAllText(@"C:\test\pierwszy.txt");
+DateTime thisDay = DateTime.Today;
+var date = thisDay.ToString("d").Replace("-", "");
+var replacement = plik.Replace("praca", "job");
+
+String path = "C:/test/pierwszy_changed-" + date + ".txt";
+File.WriteAllText(path, replacement);
